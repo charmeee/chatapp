06c51d16b57df297a19873c39bdddf3eaf385464
mj <apple8841193@naver.com>
2022-06-04 03:14:31 +0900

    22.06.03

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter,Route,Switch} from "react-router-dom";
+import LoginPage from './components/LoginPage/LoginPage';
+import RegisterPage from './components/RegisterPage/RegisterPage';
+import ChatPage from './components/ChatPage/ChatPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={LoginPage} />
+        <Route path="/register" component={RegisterPage} />
+        <Route path="/chat" component={ChatPage} />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -3,11 +3,23 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
+import { createStore ,applyMiddleware } from 'redux';
+import promiseMiddleware from 'redux-promise';
+import ReduxThunk from 'redux-thunk'
+import Reducers from './redux/reducers'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const createStoreWithMiddleware = applyMiddleware(promiseMiddleware,ReduxThunk)(createStore)
+
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={createStoreWithMiddleware(Reducers,
+      window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+      )}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
